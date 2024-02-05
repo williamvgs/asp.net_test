6099d747e4c122c09ea60cdc8d616824b9931cb1
williamvgs <wilsun30@o365.mrvgs.no>
2024-02-05 11:18:07 +0100

    v 0.1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_Hello_world.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
